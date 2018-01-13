199637ecb50733a34586af24e4e8df5e0baff9b9
Wolfgang Giersche <wgiersche@gmail.com>
2018-01-13 15:59:51 +0100

    Warning to use nodeSelector to advertise gpu capability

--- kubernetes/joincmd.sh
+++ kubernetes/joincmd.sh
@@ -3,3 +3,8 @@ echo sudo swapoff -av
 echo sudo kubeadm reset
 echo sudo $(sudo kubeadm token create --print-join-command)
 
+echo "=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!"
+echo
+echo "Don't forget to 'kubectl label nodes <yournode> capability=gpu', if you want to attract GPU-aware pods"
+echo
+echo "=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!"
